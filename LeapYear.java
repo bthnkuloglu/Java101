ed5d9bc07ccad7f8b6c196cda7a0cf72a047a425
Batuhan KULOĞLU <67811598+bthnkuloglu@users.noreply.github.com>
2024-06-28 01:27:41 +0300

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = k.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    System.out.print(year + " is a leap year.");
+                else
+                    System.out.print(year + " is not a leap year.");
+            } else
+                System.out.print(year + " is a leap year.");
+        } else
+            System.out.print(year + " is not a leap year.");
+
+    }
+}
