9dc8bec16a13aa8aa3725d2bd1f3dcb6e45546a3
Batuhan KULOĞLU <67811598+bthnkuloglu@users.noreply.github.com>
2024-07-01 14:13:49 +0300

    Add files via upload

---
+++ LoopsExamples/HarmonicSeries.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        int n, i;
+        double sum = 0;
+        System.out.print("Enter your number: ");
+        n = k.nextInt();
+        for (i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        System.out.println("Harmonic mean is: " + sum);
+    }   
+}
